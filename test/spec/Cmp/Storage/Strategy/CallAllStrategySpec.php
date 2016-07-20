c548afa76cecc24f32ab30e542da2957a1ffe4c9
Jordi Martin <jordimartin@gmail.com>
2016-07-20 17:17:20 +0200

    Added more tests

--- test/spec/Cmp/Storage/Strategy/CallAllStrategySpec.php
+++ test/spec/Cmp/Storage/Strategy/CallAllStrategySpec.php
@@ -77,4 +77,89 @@ class CallAllStrategySpec extends ObjectBehavior
         $adapter3->exists($path)->willReturn(false);
         $this->exists($path)->shouldBe(false);
     }
+
+    public function it_wraps_the_rename_call(
+        AdapterInterface $adapter1,
+        AdapterInterface $adapter2,
+        AdapterInterface $adapter3
+    ) {
+
+        $path = "/b/c";
+        $newpath = "/b/d";
+        $adapter1->rename($path, $newpath)->willReturn(true);
+        $adapter2->rename($path, $newpath)->willReturn(true);
+        $adapter3->rename($path, $newpath)->willReturn(true);
+        $this->rename($path, $newpath)->shouldBe(true);
+
+
+    }
+
+    public function it_wraps_the_delete_call(
+        AdapterInterface $adapter1,
+        AdapterInterface $adapter2,
+        AdapterInterface $adapter3
+    ) {
+
+        $path = "/b/c";
+        $adapter1->delete($path)->willReturn(true);
+        $adapter2->delete($path)->willReturn(true);
+        $adapter3->delete($path)->willReturn(true);
+        $this->delete($path)->shouldBe(true);
+
+
+    }
+
+    public function it_wraps_the_exists_call(
+        AdapterInterface $adapter1,
+        AdapterInterface $adapter2,
+        AdapterInterface $adapter3
+    ) {
+        $path = "/b/c";
+        $adapter1->exists($path)->willReturn(true);
+        $adapter2->exists($path)->willReturn(true);
+        $adapter3->exists($path)->willReturn(true);
+        $this->exists($path)->shouldBe(true);
+
+    }
+
+    public function it_wraps_the_get_and_getStream_calls(
+        AdapterInterface $adapter1,
+        AdapterInterface $adapter2,
+        AdapterInterface $adapter3
+    ) {
+
+        $path = "/b/c";
+        $contents = "hi!";
+        $adapter1->get($path)->willReturn($contents);
+        $this->get($path)->shouldBe($contents);
+        $adapter2->get($path)->shouldNotHaveBeenCalled();
+        $adapter3->get($path)->shouldNotHaveBeenCalled();
+
+        $adapter1->getStream($path)->willReturn($contents);
+        $this->get($path)->shouldBe($contents);
+        $adapter2->getStream($path)->shouldNotHaveBeenCalled();
+        $adapter3->getStream($path)->shouldNotHaveBeenCalled();
+    }
+
+
+    public function it_wraps_the_put_and_putStream_calls(
+        AdapterInterface $adapter1,
+        AdapterInterface $adapter2,
+        AdapterInterface $adapter3
+    ) {
+
+        $path = "/b/c";
+        $contents = "hi!";
+        $stream = "stream";
+        $adapter1->put($path, $contents)->willReturn(true);
+        $adapter2->put($path, $contents)->willReturn(true);
+        $adapter3->put($path, $contents)->willReturn(true);
+        $this->put($path, $contents)->shouldBe(true);
+
+        $adapter1->putStream($path, $stream)->willReturn(true);
+        $adapter2->putStream($path, $stream)->willReturn(true);
+        $adapter3->putStream($path, $stream)->willReturn(true);
+        $this->putStream($path, $stream)->shouldBe(true);
+    }
+
 }
